d446a9e4476d0d98531f2ae52d4123c6ea427603
uttammaurya18 <uttammaurya018@gmail.com>
2026-09-23 22:58:56 +0530

    Update MainActivity.java

--- app/src/main/java/com/studytracker/MainActivity.java
+++ app/src/main/java/com/studytracker/MainActivity.java
@@ -1,1 +1,16 @@
+package com.studytracker;
 
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Study Tracker App Khul Gayi!");
+        tv.setTextSize(24);
+        setContentView(tv);
+    }
+}
